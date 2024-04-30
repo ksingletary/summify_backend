3ce2b8e901d43e914f0e496d2cb2b3e29d4c49b6
ksingletary <keithsingletary@gmail.com>
2024-04-30 12:43:33 -0400

    production

--- db.js
+++ db.js
@@ -29,7 +29,7 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: getDatabaseUri(),
     ssl: {
